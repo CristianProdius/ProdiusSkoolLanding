14404715010cea39a1aa7d0ccdeacc1665bd60ec
Cristian Prodius <114479945+CristianProdius@users.noreply.github.com>
2025-03-24 01:57:30 +0200

    yeah i was dreming

--- src/app/api/subjects/route.ts
+++ src/app/api/subjects/route.ts
@@ -3,6 +3,8 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
 const prisma = new PrismaClient();
 
 export async function GET() {
